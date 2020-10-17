3ed00fd88c2ccb155c7f82f2a13feaf3cb7be65e
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-10-17 14:12:32 +0330

    app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(GAudioPanelApp());
 }
 
-class MyApp extends StatelessWidget {
+class GAudioPanelApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GAudioPanelAppState();
+}
+
+class GAudioPanelAppState extends State<GAudioPanelApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'پیشخان خوانشگران گنجور',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'پیشخان خوانشگران گنجور'),
+        builder: (BuildContext context, Widget child) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: Builder(
+              builder: (BuildContext context) {
+                return MediaQuery(
+                  data: MediaQuery.of(context).copyWith(
+                    textScaleFactor: 1.0,
+                  ),
+                  child: child,
+                );
+              },
+            ),
+          );
+        });
   }
 }
 
