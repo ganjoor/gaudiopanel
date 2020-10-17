9e227fd87b6ccee91a2249f8e9a7b294f7435dff
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-10-17 19:15:11 +0330

    basic login form skeleton

---
+++ lib/forms/login.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_overlay/loading_overlay.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  LoginFormState createState() => LoginFormState();
+}
+
+class LoginFormState extends State<LoginForm> {
+  bool _isLoading = false;
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  void _login() {
+    if (_formKey.currentState.validate()) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: LoadingOverlay(
+            isLoading: _isLoading,
+            child: Form(
+              key: _formKey,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text('پیشخان خوانشگران گنجور » ورود'),
+                ),
+                body: Builder(
+                  builder: (context) => Center(
+                    child: SingleChildScrollView(
+                      child: AutofillGroup(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextFormField(
+                              controller: _email,
+                              autofillHints: [AutofillHints.username],
+                              textDirection: TextDirection.ltr,
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'پست الکترونیکی وارد نشده است.';
+                                }
+                                return null;
+                              },
+                              onFieldSubmitted: (value) => _login(),
+                              decoration: InputDecoration(
+                                  hintText: 'پست الکترونیکی',
+                                  labelText: 'پست الکترونیکی'),
+                            ),
+                            TextFormField(
+                              controller: _password,
+                              obscureText: true,
+                              autofillHints: [AutofillHints.password],
+                              textDirection: TextDirection.ltr,
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'گذرواژه وارد نشده است.';
+                                }
+                                return null;
+                              },
+                              onFieldSubmitted: (value) => _login(),
+                              decoration: InputDecoration(
+                                  hintText: 'گذرواژه', labelText: 'گذرواژه'),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                RaisedButton.icon(
+                                  icon: Icon(Icons.login),
+                                  label: Text('ورود'),
+                                  onPressed: _login,
+                                ),
+                                RaisedButton.icon(
+                                  icon: Icon(Icons.launch),
+                                  label: Text('ثبت نام'),
+                                  onPressed: () async {
+                                    var url =
+                                        'https://museum.ganjoor.net/signup';
+                                    if (await canLaunch(url)) {
+                                      await launch(url);
+                                    } else {
+                                      throw 'خطا در نمایش نشانی $url';
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )));
+  }
+}
