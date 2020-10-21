f33c4703beee8de787546655533f559f686b4aca
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-10-21 20:20:47 +0330

    renew session API call

--- lib/services/auth-service.dart
+++ lib/services/auth-service.dart
@@ -7,6 +7,10 @@ import 'package:gaudiopanel/models/auth/logged-on-user-model.dart';
 class AuthService {
   final StorageService _storageService = StorageService();
 
+  /// Login
+  ///
+  /// returns empty string is everything is ok otherwise the error string
+  /// result is written in local storage
   Future<String> login(String username, String password) async {
     try {
       final http.Response response = await http.post(
@@ -20,6 +24,35 @@ class AuthService {
           'clientAppName': 'پیشخان خوانشهای گنجور',
           'language': 'fa-IR'
         }),
+      );
+
+      if (response.statusCode == 200) {
+        LoggedOnUserModel loginResponse =
+            LoggedOnUserModel.fromJson(json.decode(response.body));
+        await _storageService.setUserInfo(loginResponse);
+      } else {
+        return response.body;
+      }
+    } catch (e) {
+      return 'سرور مشخص شده در تنظیمات در دسترس نیست.\u200Fجزئیات بیشتر: ' +
+          e.toString();
+    }
+    return '';
+  }
+
+  /// renew user session
+  ///
+  /// returns empty string is everything is ok otherwise the error string
+  /// result is written in local storage
+  Future<String> relogin() async {
+    try {
+      LoggedOnUserModel oldLoginInfo = await _storageService.getUserInfo();
+      var sessionId = oldLoginInfo.sessionId;
+      final http.Response response = await http.put(
+        'http://localhost:3439/api/users/relogin/$sessionId',
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
       );
 
       if (response.statusCode == 200) {
