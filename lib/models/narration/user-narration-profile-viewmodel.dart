f38281cfb0df58cc55ce61a80ab110c4164d46d9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-10-28 20:03:02 +0330

    #4 get api call

---
+++ lib/models/narration/user-narration-profile-viewmodel.dart
@@ -0,0 +1,33 @@
+class UserNarrationProfileViewModel {
+  final int id;
+  final String fileSuffixWithoutDash;
+  final String artistName;
+  final String artistUrl;
+  final String audioSrc;
+  final String audioSrcUrl;
+  final bool isDefault;
+
+  UserNarrationProfileViewModel(
+      {this.id,
+      this.fileSuffixWithoutDash,
+      this.artistName,
+      this.artistUrl,
+      this.audioSrc,
+      this.audioSrcUrl,
+      this.isDefault});
+
+  factory UserNarrationProfileViewModel.fromJson(Map<String, dynamic> json) {
+    if (json == null) {
+      return null;
+    }
+    return UserNarrationProfileViewModel(
+      id: json['id'],
+      fileSuffixWithoutDash: json['fileSuffixWithoutDash'],
+      artistName: json['artistName'],
+      artistUrl: json['poemFullTitle'],
+      audioSrc: json['audioSrc'],
+      audioSrcUrl: json['audioSrcUrl'],
+      isDefault: json['isDefault'],
+    );
+  }
+}
--- lib/services/narration-service.dart
+++ lib/services/narration-service.dart
@@ -5,14 +5,18 @@ import 'package:gaudiopanel/models/auth/logged-on-user-model.dart';
 import 'package:gaudiopanel/models/common/pagination-metadata.dart';
 import 'package:gaudiopanel/models/narration/poem-narration-viewmodel.dart';
 import 'package:gaudiopanel/models/narration/poem-narrations-response-model.dart';
+import 'package:gaudiopanel/models/narration/user-narration-profile-viewmodel.dart';
 import 'package:gaudiopanel/services/auth-service.dart';
 import 'package:gaudiopanel/services/gservice-address.dart';
 import 'package:gaudiopanel/services/storage-service.dart';
 import 'package:http/http.dart' as http;
+import 'package:tuple/tuple.dart';
 
 class NarrationService {
   final StorageService _storageService = StorageService();
 
+  /// Get Narrations
+  ///
   Future<PoemNarrationsResponseModel> getNarrations(
       int pageNumber, int pageSize, bool error401) async {
     LoggedOnUserModel userInfo = await _storageService.userInfo;
@@ -48,6 +52,46 @@ class NarrationService {
           error: '');
     } else {
       return PoemNarrationsResponseModel(error: response.body);
+    }
+  }
+
+  /// Get Profiles
+  ///
+  ///returns a Tuple2, if any error occurs Items1 is null and Item2 contains the error message
+  ///Items1 is the actual response if the call is successful
+  Future<Tuple2<List<UserNarrationProfileViewModel>, String>> getProfiles(
+      bool error401) async {
+    LoggedOnUserModel userInfo = await _storageService.userInfo;
+    if (userInfo == null) {
+      return Tuple2<List<UserNarrationProfileViewModel>, String>(
+          null, 'کاربر وارد سیستم نشده است.');
+    }
+    var apiRoot = GServiceAddress.Url;
+    http.Response response =
+        await http.get('$apiRoot/api/audio/profile', headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: 'bearer ' + userInfo.token
+    });
+
+    if (!error401 && response.statusCode == 401) {
+      String errSessionRenewal = await AuthService().relogin();
+      if (errSessionRenewal.isNotEmpty) {
+        return Tuple2<List<UserNarrationProfileViewModel>, String>(
+            null, errSessionRenewal);
+      }
+      return await getProfiles(true);
+    }
+
+    List<UserNarrationProfileViewModel> ret = [];
+    if (response.statusCode == 200) {
+      List<dynamic> items = json.decode(response.body);
+      for (var item in items) {
+        ret.add(UserNarrationProfileViewModel.fromJson(item));
+      }
+      return Tuple2<List<UserNarrationProfileViewModel>, String>(ret, '');
+    } else {
+      return Tuple2<List<UserNarrationProfileViewModel>, String>(
+          null, response.body);
     }
   }
 }
