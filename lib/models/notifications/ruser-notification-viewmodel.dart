fd7423fee4e5d28d57399c2dfad339b9df8d24e4
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-11-28 18:30:31 +0330

    adding base service for notifications

---
+++ lib/models/notifications/ruser-notification-viewmodel.dart
@@ -0,0 +1,28 @@
+class NotificationStatus {
+  static const int Unread = 0;
+  static const int Read = 1;
+}
+
+class RUserNotificationViewModel {
+  final String id;
+  final String dateTime;
+  final int status;
+  final String subject;
+  final String htmlText;
+
+  RUserNotificationViewModel(
+      {this.id, this.dateTime, this.status, this.subject, this.htmlText});
+
+  factory RUserNotificationViewModel.fromJson(Map<String, dynamic> json) {
+    if (json == null) {
+      return null;
+    }
+    return RUserNotificationViewModel(
+      id: json['id'],
+      dateTime: json['dateTime'],
+      status: json['status'],
+      subject: json['subject'],
+      htmlText: json['htmlText'],
+    );
+  }
+}
---
+++ lib/services/notification-service.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:gaudiopanel/models/auth/logged-on-user-model.dart';
+import 'package:gaudiopanel/models/notifications/ruser-notification-viewmodel.dart';
+import 'package:gaudiopanel/services/auth-service.dart';
+import 'package:gaudiopanel/services/gservice-address.dart';
+import 'package:gaudiopanel/services/storage-service.dart';
+import 'package:tuple/tuple.dart';
+import 'package:http/http.dart' as http;
+
+class NotificationService {
+  final StorageService _storageService = StorageService();
+
+  /// Get Notifications
+  ///
+  ///returns a Tuple2, if any error occurs Items1 is null and Item2 contains the error message
+  ///Items1 is the actual response if the call is successful
+  Future<Tuple2<List<RUserNotificationViewModel>, String>> getProfiles(
+      String artistName, bool error401) async {
+    try {
+      LoggedOnUserModel userInfo = await _storageService.userInfo;
+      if (userInfo == null) {
+        return Tuple2<List<RUserNotificationViewModel>, String>(
+            null, 'کاربر وارد سیستم نشده است.');
+      }
+      var apiRoot = GServiceAddress.Url;
+      http.Response response =
+          await http.get('$apiRoot/api/notifications', headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: 'bearer ' + userInfo.token
+      });
+
+      if (!error401 && response.statusCode == 401) {
+        String errSessionRenewal = await AuthService().relogin();
+        if (errSessionRenewal.isNotEmpty) {
+          return Tuple2<List<RUserNotificationViewModel>, String>(
+              null, errSessionRenewal);
+        }
+        return await getProfiles(artistName, true);
+      }
+
+      List<RUserNotificationViewModel> ret = [];
+      if (response.statusCode == 200) {
+        List<dynamic> items = json.decode(response.body);
+        for (var item in items) {
+          ret.add(RUserNotificationViewModel.fromJson(item));
+        }
+        return Tuple2<List<RUserNotificationViewModel>, String>(ret, '');
+      } else {
+        return Tuple2<List<RUserNotificationViewModel>, String>(
+            null,
+            'کد برگشتی: ' +
+                response.statusCode.toString() +
+                ' ' +
+                response.body);
+      }
+    } catch (e) {
+      return Tuple2<List<RUserNotificationViewModel>, String>(
+          null,
+          'سرور مشخص شده در تنظیمات در دسترس نیست.\u200Fجزئیات بیشتر: ' +
+              e.toString());
+    }
+  }
+}
