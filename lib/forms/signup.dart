296b747ababc1a5a1fe6889675e23e915c88fdde
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-11-30 20:39:01 +0330

    #18 begining phase 3 of sign up

--- lib/forms/signup.dart
+++ lib/forms/signup.dart
@@ -15,6 +15,7 @@ class SignUpFormState extends State<SignUpForm>
     with AfterLayoutMixin<SignUpForm> {
   bool _alreadyLoggedIn = false;
   bool _emailSent = false;
+  bool _emailVerified = false;
   bool _isLoading = true;
   String _captchaImageId = '';
   String get _captchaImageUrl {
@@ -28,6 +29,10 @@ class SignUpFormState extends State<SignUpForm>
   final TextEditingController _email = TextEditingController();
   final TextEditingController _captcha = TextEditingController();
   final TextEditingController _secret = TextEditingController();
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _family = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _confirm = TextEditingController();
 
   String _signupError = '';
 
@@ -36,6 +41,10 @@ class SignUpFormState extends State<SignUpForm>
     _email.dispose();
     _captcha.dispose();
     _secret.dispose();
+    _name.dispose();
+    _family.dispose();
+    _password.dispose();
+    _confirm.dispose();
     super.dispose();
   }
 
@@ -84,6 +93,36 @@ class SignUpFormState extends State<SignUpForm>
         _formKey.currentState.validate();
       } else {
         setState(() {
+          _emailSent = true;
+          _emailVerified = true;
+          _email.text = verifyRes.item1;
+        });
+      }
+    }
+  }
+
+  void _finalize() async {
+    setState(() {
+      _signupError = '';
+    });
+    if (_formKey.currentState.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      var verifyRes = await AuthService().verifyEmail(true, _secret.text);
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (verifyRes.item2.isNotEmpty) {
+        setState(() {
+          _signupError = verifyRes.item2;
+        });
+        _formKey.currentState.validate();
+      } else {
+        setState(() {
+          _emailSent = true;
+          _emailVerified = true;
           _email.text = verifyRes.item1;
         });
       }
@@ -163,11 +202,15 @@ class SignUpFormState extends State<SignUpForm>
                             Visibility(
                                 child: Text(
                                     'لطفا پست الکترونیکی خود را بررسی کنید. در صورتی که نشانی پست الکترونیکی خود را درست وارد کرده باشید نامه‌ای از گنجور دریافت کرده‌اید که حاوی یک رمز است. '),
-                                visible: !_alreadyLoggedIn && _emailSent),
+                                visible: _emailSent && !_emailVerified),
                             Visibility(
                                 child: Text(
                                     'یا روی نشانی ارسال شده به پست الکترونیکی خود کلیک کنید یا رمز دریافتی را در کادر زیر وارد کرده روی دکمهٔ «ادامه» کلیک کنید. '),
-                                visible: !_alreadyLoggedIn && _emailSent),
+                                visible: _emailSent && !_emailVerified),
+                            Visibility(
+                                child: Text(
+                                    'لطفا نام و نام خانوادگی و رمز مد نظر خود برای ورود را وارد کنید.'),
+                                visible: _emailVerified),
                             SizedBox(width: 10),
                             Visibility(
                               child: Image.network(_captchaImageUrl),
@@ -188,13 +231,49 @@ class SignUpFormState extends State<SignUpForm>
                                     }
                                     return null;
                                   },
-                                  onFieldSubmitted: (value) => _signup(),
+                                  onFieldSubmitted: (value) => _verify(),
                                   decoration: InputDecoration(
                                       prefix: Icon(Icons.lock),
                                       hintText: 'رمز دریافتی را وارد نمایید',
                                       labelText: 'رمز دریافتی'),
                                 ),
-                                visible: !_alreadyLoggedIn && _emailSent),
+                                visible: !_alreadyLoggedIn &&
+                                    _emailSent &&
+                                    !_emailVerified),
+                            Visibility(
+                                child: TextFormField(
+                                  controller: _name,
+                                  validator: (value) {
+                                    if (_emailVerified && value.isEmpty) {
+                                      return 'لطفا نام خود را وارد نمایید.';
+                                    }
+
+                                    return null;
+                                  },
+                                  onFieldSubmitted: (value) => _finalize(),
+                                  decoration: InputDecoration(
+                                      prefix: Icon(Icons.person),
+                                      hintText: 'نام',
+                                      labelText: 'نام'),
+                                ),
+                                visible: _emailVerified),
+                            Visibility(
+                                child: TextFormField(
+                                  controller: _family,
+                                  validator: (value) {
+                                    if (_emailVerified && value.isEmpty) {
+                                      return 'لطفا نام خانوادگی خود را وارد نمایید.';
+                                    }
+
+                                    return null;
+                                  },
+                                  onFieldSubmitted: (value) => _finalize(),
+                                  decoration: InputDecoration(
+                                      prefix: Icon(Icons.person),
+                                      hintText: 'نام خانوادگی',
+                                      labelText: 'نام خانوادگی'),
+                                ),
+                                visible: _emailVerified),
                             SizedBox(width: 10),
                             Visibility(
                                 child: TextFormField(
@@ -224,7 +303,59 @@ class SignUpFormState extends State<SignUpForm>
                                           color: Colors.green,
                                           onPressed: _verify,
                                         ))),
-                                visible: !_alreadyLoggedIn && _emailSent),
+                                visible: _emailSent && !_emailVerified),
+                            Visibility(
+                                child: TextFormField(
+                                  controller: _password,
+                                  obscureText: true,
+                                  autofillHints: [AutofillHints.password],
+                                  textDirection: TextDirection.ltr,
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'گذرواژه وارد نشده است.';
+                                    }
+                                    if (_signupError.isNotEmpty) {
+                                      return _signupError;
+                                    }
+                                    return null;
+                                  },
+                                  onFieldSubmitted: (value) => _finalize(),
+                                  decoration: InputDecoration(
+                                      icon: Icon(Icons.lock),
+                                      hintText: 'گذرواژه',
+                                      labelText: 'گذرواژه'),
+                                ),
+                                visible: _emailVerified),
+                            Visibility(
+                                child: TextFormField(
+                                  controller: _confirm,
+                                  obscureText: true,
+                                  autofillHints: [AutofillHints.password],
+                                  textDirection: TextDirection.ltr,
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'تکرار گذرواژه وارد نشده است.';
+                                    }
+                                    if (value != _password.text) {
+                                      return 'تکرار گذرواژه همخوانی ندارد.';
+                                    }
+                                    return null;
+                                  },
+                                  onFieldSubmitted: (value) => _finalize(),
+                                  decoration: InputDecoration(
+                                      icon: Icon(Icons.lock),
+                                      hintText: 'تکرار گذرواژه',
+                                      labelText: 'تکرار گذرواژه'),
+                                ),
+                                visible: _emailVerified),
+                            Visibility(
+                                child: Text(
+                                    'گذرواژه باید شامل ۶ حرف باشد و از ترکیبی از اعداد و حروف انگلیسی تشکیل شده باشد.'),
+                                visible: _emailVerified),
+                            Visibility(
+                                child: Text(
+                                    'حروف و اعداد نباید تکراری باشند و وجود حداقل یک عدد و یک حرف کوچک انگلیسی در گذرواژه الزامی است.'),
+                                visible: _emailVerified),
                             SizedBox(
                               height: 10.0,
                             ),
@@ -240,6 +371,18 @@ class SignUpFormState extends State<SignUpForm>
                                           onPressed: _signup,
                                         ))),
                                 visible: !_alreadyLoggedIn && !_emailSent),
+                            Visibility(
+                                child: SizedBox(
+                                    width: double.maxFinite,
+                                    child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: RaisedButton.icon(
+                                          icon: Icon(Icons.launch),
+                                          label: Text('ادامه'),
+                                          color: Colors.green,
+                                          onPressed: _finalize,
+                                        ))),
+                                visible: _emailVerified),
                             Visibility(
                               child: Text('شما پیش‌تر ثبت نام کرده‌اید!'),
                               visible: _alreadyLoggedIn,
